7d695ad2a9242c30f84d2ef4c5709fe09af3a803
Matusek Márton <matusek.marton@autsoft.hu>
2020-12-15 00:12:16 +0100

    Add forward auth

--- user-api/src/main/java/hu/aut/bme/userapi/feature/controller/AuthController.java
+++ user-api/src/main/java/hu/aut/bme/userapi/feature/controller/AuthController.java
@@ -1,25 +1,28 @@
 package hu.aut.bme.userapi.feature.controller;
 
-import org.springframework.data.repository.query.Param;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Arrays;
 import java.util.List;
 
+import static org.springframework.web.bind.annotation.RequestMethod.GET;
+import static org.springframework.web.bind.annotation.RequestMethod.POST;
+
 @RestController
 @RequestMapping("/api/auth")
 public class AuthController {
     private final List<String> allowedUsers = Arrays.asList("admin1", "admin2", "blogger1");
 
-    @RequestMapping
-    public ResponseEntity auth(@Param("username") String username) {
-        if (!allowedUsers.contains(username)) {
-            return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
+    @RequestMapping(method = {GET, POST})
+    public ResponseEntity auth(@RequestHeader("X-Forwarded-User") String user) {
+        if (allowedUsers.contains(user)) {
+            return new ResponseEntity(HttpStatus.OK);
         }
-        return new ResponseEntity(HttpStatus.OK);
+        return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
     }
 
 }
